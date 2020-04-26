231962ba2e2dc2119f5594233b6fa4eff0313177
mrzrashed <mrzrashed>
2020-04-26 17:09:44 +0600

    Cart Language

--- resources/lang/en-US/cart.php
+++ resources/lang/en-US/cart.php
@@ -9,5 +9,15 @@ return [
     'Sub Total' => 'Sub Total',
     'Confirm' => 'Confirm Order',
     'Kg'=>'KG',
+    'Action'=>'Action',
+    'Save for Later'=>'Save for Later',
+    'Continue Shopping'=>'Continue Shopping',
+    'Have a Code?'=>'Have a Code?',
+    'Apply'=>'Apply',
+    'Move to Cart'=>'Move to Cart',
+    'There is no items in your cart!'=>'There is no items in your cart!',
+    'Shop Now'=>'Shop Now',
+    'Saved For Later'=>'Saved For Later',
+    'item(s)'=>'item(s)',
 ];
 ?>
