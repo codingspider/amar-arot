6607a7d8fe98adf1cda5c4a01a3de2fe12911b71
shuvo575 <shohaghasan575@gmail.com>
2020-05-08 22:51:25 +0600

    Icon Updeated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
-    <title>@yield('pagetitle','Optima')</title>
+    <title>@yield('pagetitle','AmarArot')</title>
 
     <!-- CSS  -->
     <!-- datatblae -->
@@ -16,7 +16,7 @@
 
     <!-- PWA Links -->
 
-    <link rel="icon" href="{{asset('content')}}/img/favicon.ico" type="image/x-icon" />
+    <link rel="icon" href="{{asset('content')}}/img/fav.png" type="image/png" />
 
     <!-- CODELAB: Add link rel manifest -->
     <link rel="manifest" href="{{asset('manifest.json')}}">
