a67e3ff276b9a1a05c0efc973bdf3a32f1300258
mrzrashed <mrzrashed>
2020-05-08 17:05:32 +0600

    phone on registration

--- app/User.php
+++ app/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'name_bn', 'email', 'password',
+        'name', 'name_bn', 'email', 'password','phone',
     ];
 
     /**
